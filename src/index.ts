b0e15dec914575596966e8eca56db2958c730cd6
MoonSoD <moonsod8@gmail.com>
2021-08-06 22:09:19 +0200

    Command module

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import {Client} from 'discord.js';
+import {AModule, CommandRegistry} from "./commands/command-registry";
 
 require('dotenv').config()
 
-const client = new Client()
+export const client = new Client()
 
 client.once("ready", () => {
     console.log("Armed and ready!")
 });
 
+const modules: AModule[] = [
+    new CommandRegistry()
+]
+
 client.login(process.env.BOT_TOKEN).then(() => {
     console.log("Logged in!")
+    modules.forEach(({register}) => register())
 })
