101d5ea6ef514c29c1f293381490baa3a5aae30a
MoonSoD <moonsod8@gmail.com>
2021-08-06 20:54:15 +0200

    Everything

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import {Client} from 'discord.js';
 
+require('dotenv').config()
+
 const client = new Client()
 
 client.once("ready", () => {
     console.log("Armed and ready!")
 });
 
-client.login("")
+client.login(process.env.BOT_TOKEN).then(() => {
+    console.log("Logged in!")
+})
